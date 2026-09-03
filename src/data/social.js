3e73e033a719a0dcd2564ef413771ef85b234f82
pavithrarapuru <pavithrarapuru@gmail.com>
2026-09-03 17:09:38 +0530

    about.jsx

--- src/data/social.js
+++ src/data/social.js
@@ -1,7 +1,19 @@
 const socialLinks = [
-  { label: 'hello@example.com', icon: 'email', href: 'mailto:hello@example.com' },
-  { label: 'LinkedIn', icon: 'web', href: 'https://www.linkedin.com/in/pavithrarapuru3154' },
-  { label: 'GitHub', icon: 'code', href: 'https://github.com/Pavithrarapuru' },
+  {
+    label: 'pavithrarapuru@gmail.com',
+    icon: 'email',
+    href: 'mailto:pavithrarapuru@gmail.com',
+  },
+  {
+    label: 'LinkedIn',
+    icon: 'web',
+    href: 'https://www.linkedin.com/in/pavithrarapuru3154',
+  },
+  {
+    label: 'GitHub',
+    icon: 'code',
+    href: 'https://github.com/Pavithrarapuru',
+  },
 ]
 
 export default socialLinks
